84ff829d1acaa70b6628823155175abc41232aa8
Naomi Hegwood <nomes@dhcp-10-250-246-83.harvard.edu>
2021-07-19 16:02:06 -0400

    Added code to separate emergents from nonemergents and for t tests

--- Danum_code/dan_surr.R
+++ Danum_code/dan_surr.R
@@ -16,7 +16,38 @@ library(plyr)
 
 surdat <- read_csv("~/Desktop/Research/HCRP/dan_dat/dan_surr.csv")
 
-summary(surdat)
+plot(surdat$plot_x,surdat$plot_y)
+emergsurdat <- filter(surdat, dbh >= 95)
+plot(emergsurdat$plot_x,emergsurdat$plot_y)
+nonsurdat <- filter(surdat, dbh < 95)
+plot(nonsurdat$plot_x,nonsurdat$plot_y)
 
-lm.helev <- lm(height~heightmean, data=surdat)
-summary(lm.helev)
+summary(nonsurdat)
+summary(emergsurdat)
+
+t.test(nonsurdat$heightmean,emergsurdat$heightmean)
+
+t.test(nonsurdat$n_trees,emergsurdat$n_trees)
+
+t.test(nonsurdat$heightmean,emergsurdat$heightmean)
+
+
+#---------------------------------------------------------------------------------------------#
+#----------------------------Surrounding Tree Analysis Dataset--------------------------------
+#-------------------------------------sampling >=60 cm----------------------------------------#
+
+surdat60 <- read_csv("~/Desktop/Research/HCRP/dan_dat/dan_surr60.csv")
+plot(surdat60$plot_x,surdat60$plot_y)
+
+emergsurdat60 <- filter(surdat60, dbh >= 95)
+nonsurdat60 <- filter(surdat60, dbh < 95)
+
+
+summary(nonsurdat60)
+summary(emergsurdat60)
+
+t.test(nonsurdat60$heightmean,emergsurdat60$heightmean)
+
+t.test(nonsurdat60$n_trees,emergsurdat60$n_trees)
+
+t.test(nonsurdat60$heightmean,emergsurdat60$heightmean)
