9ccd8832dcaa4f655f2231a9e791b0aa5ed2c4c6
Naomi Hegwood <nomes@dhcp-10-250-108-13.harvard.edu>
2022-06-10 10:32:17 -0400

    Attempt to fix issue

--- GEDI/Danum/buffer_Danum.R
+++ GEDI/Danum/buffer_Danum.R
@@ -12,30 +12,47 @@ library(plyr)
 library(rgeos)
 require("maptools")
 
+# load data
+# gedi
 dat <- read_csv("~/Desktop/Research/Danum_GEDIdata/dan_GEDIdat.csv")
 summary(dat$Longitude)
 dat$id <- seq.int(nrow(dat)) 
-
+# elev
+Danum_elev <- raster("~/Google Drive/My Drive/Research/elevdat/elevdat.tif")
+NAvalue(Danum_elev) <- -9999
+plot(Danum_elev)
+# plot(Danum_elev) 
+# twi
 Danum_TWI <- raster("~/Desktop/Research/HCRP/dan_dat/Danum_TWI.tif")
-dan_proj <- crs(Danum_TWI)
 
-#-1 
-coords<- dat[,c("Latitude","Longitude")]
+# Make spatial points dataframe with gedi data
+dan_proj = crs(Danum_elev)
+dan_proj
+coords<- dat[,c("Longitude","Latitude")]
 dandat <- SpatialPointsDataFrame(coords=coords,
-                                               data=dat,
-                                               proj4string=dan_proj)
-
+                                        data=dat,
+                                        proj4string=dan_proj)
+crs(dandat) = crs(Danum_TWI)
 plot(dandat)
+
+#test extracting data
+test <- raster::extract(Danum_elev, dandat)
+head(test)
+summary(test)
+length(test)
+dim(spatialdan)
+
+# make buffer
 dandat_buff <- gBuffer(dandat, width=.0025, byid = TRUE) #EO: use the gBuffer function in rgeos instead, and set byid=T
 plot(dandat_buff)
 
 spatialdan <- dandat_buff
-#elev------ 
-Danum_elev <- raster("~/Google Drive/My Drive/Research/elevdat/elevdat.tif"); 
 
-e <- as(extent(116.492871, 117.820664, 3.849471, 6.094), 'SpatialPolygons')
-crs(e) <- crs(Danum_elev)
-delev <- crop(Danum_elev, e)
+# code to crop danum elev but isn't working?
+#e <- as(extent(116.492871, 118.820664, 3.849471, 6.094), 'SpatialPolygons')
+#crs(e) <- crs(Danum_elev)
+
+#delev <- crop(Danum_elev, e)
 crs(spatialdan) = crs(Danum_elev)
 plot(delev)
 
@@ -45,7 +62,10 @@ plot(spatialdan)
 
 #extract data
 #elev
-test <- raster::extract(Danum_elev,spatialdan)
+crs(Danum_elev)
+crs(spatialdan)
+test <- raster::extract(Danum_elev,dandat)
+plot(Danum_elev)
 head(test)
 summary(test)
 length(test)
@@ -71,3 +91,16 @@ head(temp)
 length(temp)
 dim(spatialdan)
 analysis$tpi <- raster::extract(Danum_slope_aspect_TPI$tpi,spatialdan)
+
+# TWI
+crs(Danum_TWI)
+crs(spatialdan)
+
+test <- raster::extract(Danum_TWI,spatialdan, method = "bilinear")
+head(test)
+summary(test)
+length(test)
+dim(spatialdan)
+analysis <- as(spatialdan, "data.frame")
+analysis$elev <- raster::extract(Danum_TWI,spatialdan, method = "bilinear")
+
