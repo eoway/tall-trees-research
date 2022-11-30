6e7b4f37fa916ac06f7c66ff005e841292148610
Naomi Hegwood <70771598+nhegwood@users.noreply.github.com>
2022-11-29 23:05:20 -0500

    ecoregions code

--- Thesis/Chapter_2/ecoregions.R
+++ Thesis/Chapter_2/ecoregions.R
@@ -16,12 +16,21 @@ library(plotKML)
 # read in ecoregions
 ecoregions <- readOGR('~/Desktop/Research_2022/Data/Ecoregions',"wwf_terr_ecos")
 ecoregions1 <- subset(ecoregions, ecoregions$BIOME == "1")
-plot(ecoregions1)
-crs(ecoregions1)
+extent(ecoregions1)
+writeOGR(ecoregions1, dsn = "~/Desktop/Research_2022/Data/Ecoregions", layer = "ecoregions_biome_1",
+         driver = "ESRI Shapefile" )
 
-ecoregions2 <- ecoregions1@polygons
+# in arcgis tool called dissolve, may not need to do, shouldn't need to do
+# crop out madagascar, keep in continental Africa and southeast asia
+# Can do: - figure out what extent is and manually list out corners - create a box shapefile from ecoregion
+# make one bounding box for africa and one for southeast asia
 
-ecoregions2 %>% 
-  group_by(BIOME) %>%
+ecoregions2 <- crop(ecoregions1, extent(-90, 150, -34, 30))
+extent(ecoregions1)
+plot(ecoregions2)
+
+
+ecoregions1 %>% 
+  dplyr::group_by(OBJECTID) %>%
   summarise(geometry = sf::st_union(geometry)) %>%
   ungroup()
